2cf4d295b7d2ac3a6dd75c9c2e022815a2ef3b44
Samuel Ortiz <sdortizm@gmail.com>
2026-04-01 23:08:26 -0600

    fix(flutter): restaurar URL de producción en ApiService

--- flutter_app/lib/services/api_service.dart
+++ flutter_app/lib/services/api_service.dart
@@ -5,6 +5,7 @@ import '../models/historial_precio.dart';
 
 class ApiService {
   static const String baseUrl = 'https://comparador-precios-gt.onrender.com';
+  // static const String baseUrl = 'http://10.0.2.2:8000'; // emulador Android
 
   static Future<List<Producto>> buscarProductos(String query) async {
     final uri = Uri.parse('$baseUrl/buscar?q=${Uri.encodeComponent(query)}');
